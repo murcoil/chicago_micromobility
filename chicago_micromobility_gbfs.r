aac0763fee7ce8d81c3b8635137e70e7a35ae418
Colin Murphy <murcoil@gmail.com>
2020-08-03 15:46:29 -0500

    added saving a csv of status snapshot

--- chicago_micromobility_gbfs.r
+++ chicago_micromobility_gbfs.r
@@ -6,6 +6,8 @@ library(gbfs)
 library(tidyverse)
 
 divfeed <- "https://gbfs.divvybikes.com/gbfs/gbfs.json"
+
+
 div_station_info <- get_station_information(divfeed)
 div_station_status <- get_station_status(divfeed)
 
@@ -27,14 +29,15 @@ div_free_bikes <- get_free_bike_status(divfeed, output = "return") %>%
         # just select columns we're interested in visualizing
         select(id = bike_id, lon, lat, type) %>%
         # make columns analogous to station_status for row binding
-        mutate(num_ebikes_available = 1,
-               num_docks_available = NA,
-               type = "free")
+        # this assumes that all free floating bikes are e-bikes but that's not necessarily the case
+        mutate(num_bikes_available = 1,
+               num_ebikes_available = 1,
+               num_docks_available = NA)
 
 # row bind stationed and free bike info
 div_full <- bind_rows(div_stations, div_free_bikes)
 
-# filter out stations with 0 available bikes
+# filter out stations with 0 available e-bikes
 div_plot <- div_full %>%
         filter(num_ebikes_available > 0) %>%
         # plot the geospatial distribution of bike counts
@@ -47,4 +50,9 @@ div_plot <- div_full %>%
         # make aesthetics slightly more cozy
         theme_minimal() +
         scale_color_brewer(type = "qual")
-div_plot
+
+# uncomment the next line to produce the plot
+# div_plot
+
+# uncomment the next line to save a timestamped csv in the working directory
+# write.csv(div_full,paste0("divvystatus",format(Sys.time(), "_%Y%m%d_%H%M%S"),".csv"), row.names = FALSE)
