8cb9d341bd10c7da4906f48159bea497154201ed
Colin Murphy <murcoil@gmail.com>
2020-08-05 14:06:19 -0500

    added comments, added scooter feeds for later

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A very simple tool for looking at GBFS feeds in Chicago.
 
 Running the script 'chicago_micromobility_gbfs.r' in R will leave you with an object, div_full, that lists all the station statuses, including e-bike counts, across the Divvy system, as well as locations of free-floating e-Divvys. (Right now, it assumes that any free-floating bikes it sees are electric but that may not always be the case.)
 
-The last few lines have commands to make a map and save a time-stamped csv. 
+The last few lines have commands to make a map like the one below and save a time-stamped csv from this data. 
 
 ![Free Divvy E-bikes](/images/free-edivvy.png)
 
--- chicago_micromobility_gbfs.r
+++ chicago_micromobility_gbfs.r
@@ -1,31 +1,37 @@
-## Script to look at GBFS feeds in Chicago
-## Divvy endpoint: https://gbfs.divvybikes.com/gbfs/gbfs.json
+## Script to look at GBFS feeds for micromobility in Chicago
 ## based on gbfs tools at https://github.com/simonpcouch/gbfs
-## more ways to visualize things https://www.rdocumentation.org/packages/ggmap/versions/3.0.0
 
 library(gbfs)
 library(tidyverse)
 library(ggmap)
 
+## store the URL for the feed(s) of interest
 divfeed <- "https://gbfs.divvybikes.com/gbfs/gbfs.json"
 
+## Just leaving these here, we'll need them soon
+# birdfeed <- "https://mds.bird.co/gbfs/chicago/gbfs.json"
+# spinfeed <- "https://web.spin.pm/api/gbfs/v1/chicago/gbfs.json"
+# limefeed <- "https://data.lime.bike/api/partners/v1/gbfs/chicago/gbfs.json"
+
+
+## grab data from station endpoints
 div_station_info <- get_station_information(divfeed)
 div_station_status <- get_station_status(divfeed)
 
-# grab data on free bike status and save it as a dataframe
+## grab data on free bike status and save it as a dataframe
 div_free_bikes <- get_free_bike_status(divfeed, output = "return") %>%
         # just select columns we're interested in visualizing
         select(id = bike_id, lon, lat, type) %>%
         # make columns analogous to station_status for row binding
-        # this assumes that all free floating bikes are e-bikes but that's not necessarily the case; they will show up as another 'type' if they do
-        mutate(num_bikes_available = 1,
+        # this assumes that all free floating bikes are e-bikes but that's not necessarily the case; they will show up as another 'type' if they do and we'll have to change things up here a bit
+        mutate(num_bikes_available = 0,
                num_ebikes_available = 1,
                num_docks_available = NA)
 
-# store the last time the feed was fetched
+# store the time the feed was fetched
 feedtime <- Sys.time()
 
-# join station datasets on station_id and select a few columns
+## join station datasets on station_id and select a few columns
 div_stations <- full_join(div_station_info,
                           div_station_status,
                           by = "station_id") %>%
@@ -38,10 +44,10 @@ div_stations <- full_join(div_station_info,
                num_docks_available) %>%
         mutate(type = "docked")
 
-# row bind stationed and free bike info
+## row bind stationed and free bike info
 div_full <- bind_rows(div_stations, div_free_bikes)
 
-# filter out stations with 0 available e-bikes
+## filter out stations with 0 available e-bikes, and build a plot
 # theme_set(theme_bw())
 div_plot <- div_full %>% #filter(num_ebikes_available > 0) %>%
         # plot the geospatial distribution of bike counts
@@ -54,9 +60,10 @@ div_plot <- div_full %>% #filter(num_ebikes_available > 0) %>%
                 size = num_ebikes_available) +
         scale_size_area("E-bikes available")
 
-# uncomment the next line to produce the plot
-# div_plot
-#
+## more ways to visualize things https://www.rdocumentation.org/packages/ggmap/versions/3.0.0
 
-# uncomment the next line to save a timestamped csv in the working directory
+## uncomment the next line to produce the plot
+# div_plot
+
+## uncomment the next line to save a timestamped csv in the working directory
 # write.csv(div_full,paste0("divvystatus",format(feedtime, "_%Y%m%d_%H%M%S"),".csv"), row.names = FALSE)
